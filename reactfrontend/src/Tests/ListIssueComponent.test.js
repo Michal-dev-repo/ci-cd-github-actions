c1ae6aed5e11a3dd6a56e055ac98ba97e884464d
mp <123307575+Michal-dev-repo@users.noreply.github.com>
2023-02-25 20:51:01 +0100

    added backend

--- reactfrontend/src/Tests/ListIssueComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from '../App'
-
-test('renders issues', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/issues/i)
-  expect(linkElement).toBeInTheDocument()
-})
